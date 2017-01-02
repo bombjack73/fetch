50cef912672808ec90e8ba51ea96853e5cde0a57
Marc Vandenbosch <marc.vandenbosch@gmail.com>
2017-01-02 20:02:23 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+fetch
+=====
+go on steroid
+-------------
---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright © 2017 Marc Vandenbosch
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package main
+
+import "github.com/bombjack73/fetch/cmd"
+
+func main() {
+	cmd.Execute()
+}
