cb9b36c82ba6cf12cb349ce22995e8b1d61891db
Marc Vandenbosch <marc.vandenbosch@gmail.com>
2017-01-03 20:38:50 +0000

    added json parsing structs

---
+++ ROADMAP.md
@@ -0,0 +1,4 @@
+Currently in no particular order:
+- vendoring through vendor directory
+- recall for next result
+- other repo than github
--- cmd/search.go
+++ cmd/search.go
@@ -6,10 +6,24 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+type GitHubRepository struct {
+	Name        string `json:"name"`
+	FullName    string `json:"full_name"`
+	Description string `json:"description"`
+	HtmlUrl     string `json:"html_url"`
+	Fork        bool   `json:"fork"`
+}
+
+type GitHubSearchResult struct {
+	Items []GitHubRepository `json"items"`
+}
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
@@ -23,7 +37,26 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("search called")
+		for _, arg := range args {
+			fmt.Println("search called: " + arg)
+			//default: in:name,description
+			//user:username in case of username/repo
+			//+fork:true
+			resp, err := http.Get("https://api.github.com/search/repositories?q=" + arg + "+language:go&order=desc")
+			//todo see err handling in go source
+			if err != nil {
+				fmt.Println("error") //todo explain
+				continue
+			}
+			defer resp.Body.Close()
+
+			res := new(GitHubSearchResult)
+			json.NewDecoder(resp.Body).Decode(res)
+			fmt.Println(len(res.Items))
+			for i := 0; i < len(res.Items); i++ {
+				fmt.Printf("%+v\n", res.Items[i])
+			}
+		}
 	},
 }
 
